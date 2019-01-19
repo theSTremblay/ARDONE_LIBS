9bf970857e859d81094416ccffe0de9c8eb2400c
Sean Tremblay <seantremblay@utexas.edu>
2019-01-19 00:51:47 -0600

    Add files via upload

--- TEST_DESTINATION_PATHING.py
+++ TEST_DESTINATION_PATHING.py
@@ -9,9 +9,59 @@ found in the file LICENSE in this distribution.
 
 # THis demo is to prove the ability of the drone to move towards a destination, the parameters will bbe a point to move towards and the drone must do its best to get there.
 import pygame
-
+import subprocess
 from pydrone import libardrone
 import cv2
+
+file = open('canRead.txt', 'w+')
+canRead = file.write('0')
+file.close()
+
+def is_square(apositiveint):
+  x = apositiveint // 2
+  seen = set([x])
+  while x * x != apositiveint:
+    x = (x + (apositiveint // x)) // 2
+    if x in seen: return False
+    seen.add(x)
+  return True
+
+def crop2(infile,imgheight,imgwidth,imgheight2, imgwidth2):
+    #im = Image.open(infile)
+    #imgwidth, imgheight = im.size
+    b= []
+    for i in range(imgheight//imgheight2):
+        for j in range(imgwidth//imgwidth2):
+            crop_img = infile[i*imgheight2:(i+1)*imgheight2, j*imgwidth2:(j+1)*imgwidth2]
+            b.append(crop_img)
+    return b
+
+def crop(im, k):
+    k2 = 9
+
+    boolean_var = is_square(k)
+    #im = Image.open(input)
+    # 290 , 640
+    imgheight, imgwidth, chan = im.shape
+
+    imgheight = int(imgheight - (imgheight % sqrt(k)))
+    imgwidth = int(imgwidth - (imgwidth % sqrt(k)))
+
+    imgheight2 = int(int(imgheight - (imgheight % sqrt(k))) / 3)
+    imgwidth2 = int(int(imgwidth - (imgwidth % sqrt(k))) / 3)
+
+    rim = cv2.resize(im, (imgwidth, imgheight))
+
+    M = rim.shape[0] // 2
+    N = rim.shape[1] // 2
+
+    # rimg = Image.fromarray(rim, 'L')
+
+    # 96, 213 dimensions of tile
+
+    tiles = crop2(rim, imgheight, imgwidth, imgheight2, imgwidth2)
+    return tiles
+
 
 
 if __name__ == '__main__':
@@ -106,58 +156,20 @@ if __name__ == '__main__':
 
                             i = 1
 
-                            file = open('DroneRead.txt', 'w+')
-                            file.write("1")
-                            droneRead = 0
-                            file.close()
-
                             for image in image_grid_array:
 
                                 cv2.imwrite("test_img.jpeg", image)
 
                                 canRead = '0'
                                 # Test for I/O file close permissions
-                                while canRead == "0":
-                                    file = open('canRead.txt', 'w+')
-                                    file.write("1")
-                                    file.close()
-                                    file = open('canRead.txt', 'r+')
-                                    canRead = str(file.readline())
-                                    print
-                                    "Can Read Succeeded to make 1"
+                                number_variable = str(20)
+                                camera_process = subprocess.Popen(["python3", "DRONE_LENET_PREDICTION.py"],
+                                                                  stdout=subprocess.PIPE)
+                                done_correct = camera_process.communicate()[0]
 
-                                    file.close()
+                                collision = float(done_correct)
 
-                                    # To run this in python 3.5 we will need to run a script that uses the "env"
-                                    # wrapper
-                                    # Once the pipeline is established, have the thread continuousely generate new samples
-                                    #
-                                    # can definitely recursion this size_of grid /2 + 1
-
-                                while (droneRead != '1'):
-                                    file = open('DroneRead.txt', 'r')
-                                    droneRead = str(file.readline())
-                                    file.close()
-
-                                while (droneRead == '1'):
-
-                                    try:
-
-                                        file = open('out.txt', 'r')
-                                        collision = float(file.readline().split(":")[1])
-                                        print("COLLISION: " + str(collision))
-                                        file.close()
-                                    except:
-                                        collision = 0
-                                        print("COLLISION: -1")
-
-                                    file = open('DroneRead.txt', 'w+')
-                                    file.write("0")
-                                    droneRead = str(file.readline())
-
-                                    file.close()
-
-                                    print("Drone Read inactive")
+                                print("Drone Read inactive")
 
                                 if i % grid_length < (size_of_grid / 2 + 1):
                                     Pl += collision
